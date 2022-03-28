93279e7944f391e7062d4290f11228bad6ee20dd
Saiful Nakeusa <47083024+nakeusa12@users.noreply.github.com>
2022-03-28 11:08:41 +0700

    ADD: firt time

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import tw, { styled } from "twin.macro";
 
 export default function Home() {
   return (
@@ -11,59 +11,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="">
+        <Button>In styled</Button>
+        <br />
+        <TailwindButton>In Tailwind Style</TailwindButton>
+        <br />
+        <ConditionalButton isRed={true}>Conditional Tailwind</ConditionalButton>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
+
+const Button = styled.div`
+background: red;
+color: white;
+font-size: 1em;
+text-align: center;
+padding: 0.25em 1em;
+border: 2px solid black;
+`
+
+const TailwindButton = tw.button`
+  bg-red-500
+  hover:bg-red-700
+  text-white
+  font-bold
+  py-2
+  px-4
+  border
+  border-black
+  rounded
+`;
+
+const ConditionalButton = styled.button(({ isRed }) => [
+  isRed ? tw`bg-red-500 hover:bg-red-700` : tw`bg-blue-500 hover:bg-blue-500`,
+  tw`px-4 py-2 font-bold text-white border border-black rounded `,
+]);

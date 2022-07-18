fd616f0c74ba7d64887e8b53a3afe3532ca0bec5
nakeusa12 <snakeusa8@gmail.com>
2022-07-18 19:32:42 +0700

    fix

--- components/partials/Hero/square.jsx
+++ components/partials/Hero/square.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Square = () => {
     const [move, setMove] = useState({ ix: 0, iy: 0 });
+    var moveSquare = useRef();
 
     // const moveSquare = (e) => {
     //     let move = document.querySelectorAll(".move");
@@ -16,19 +17,20 @@ export const Square = () => {
     //     // console.log("Test: ", e);
     //     console.log('Detected: ', move);
     // }
-    
-    const getMousePos = e => {
-        return { x: e.clientX, y: e.clientY };
-    };
-    
+
     const handleMove = e => {
-        console.log(getMousePos(e));
+        const x = (window.innerWidth - e.mouse.x ) / 120;
+        const y = (window.innerHeight - e.clientY ) / 120;
     };
+
+    useEffect((e) => {
+        console.log(handleMove(e));
+    }, [])
   
 
     return (
-        <div className="w-full min-h-screen h-full flex items-center"  onMouseMove={e => handleMove(e)} >
-            <svg viewBox="0 0 1582 747" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full max-w-[1550px] mx-auto ">
+        <div className="w-full min-h-screen h-full flex items-center" onMouseMove={e => handleMove(e)} >
+            <svg viewBox="0 0 1582 747" fill="none" xmlns="http://www.w3.org/2000/svg" ref={(el) => moveSquare = el} className="w-full h-full max-w-[1550px] mx-auto ">
                 <rect data-speed="2.6" className="move" x="21.825" y="574.827" width="68.4134" height="62.5457" rx="10" transform="rotate(-28.5616 21.825 574.827)" fill="#D9D9D9" fillOpacity="0.6" />
                 <rect data-speed="2" className="move" x="5.45166" y="181.283" width="24.1656" height="23.4325" rx="3" transform="rotate(13.4534 5.45166 181.283)" fill="white" fillOpacity="0.2" />
                 <rect data-speed="-2" className="move" x="494.397" y="522" width="31.8563" height="30.8899" rx="3" transform="rotate(38.2273 494.397 522)" fill="white" fillOpacity="0.2" />

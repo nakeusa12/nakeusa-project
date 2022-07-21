e54fe5df5392380fec1b2a7789b949053412a732
nakeusa12 <snakeusa8@gmail.com>
2022-07-21 13:38:47 +0700

    grid

--- components/partials/Hero/square.jsx
+++ components/partials/Hero/square.jsx
@@ -23,17 +23,19 @@ export const Square = () => {
         moveSet.forEach((layer) => {
             const speed = layer.getAttribute("data-speed");
 
-            const xScreen = ( e.pageX * speed ) / 250;
-            const yScreen = ( e.pageY * speed ) / 250;
+            const xScreen = ( e.screenX * speed ) / 250;
+            const yScreen = ( e.screenY * speed ) / 250;
             layer.style.transform = `translateX(${xScreen}px) translateY(${yScreen}px)`;
-            console.log("speed", speed);
+            console.log("speed: ", speed);
+            console.log("xScreen: ", e.screenX);
+            console.log("yScreen: ", e.screenY);
         })
 
     };  
 
     return (
         <div className="w-full min-h-screen h-full flex items-center transition-transform duration-300" onMouseMove={e => handleMove(e)} >
-            {/* <svg viewBox="0 0 1582 747" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full" ref={(el) => moveSquare = el}>
+            <svg viewBox="0 0 1582 747" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full" ref={(el) => moveSquare = el}>
                 <rect data-speed="-2" className="move" x="21.825" y="574.827" width="68.4134" height="62.5457" rx="10" transform="rotate(-28.5616 21.825 574.827)" fill="#D9D9D9" fillOpacity="0.6" />
                 <rect data-speed="6" className="move" x="5.45166" y="181.283" width="24.1656" height="23.4325" rx="3" transform="rotate(13.4534 5.45166 181.283)" fill="white" fillOpacity="0.2" />
                 <rect data-speed="4" className="move" x="494.397" y="522" width="31.8563" height="30.8899" rx="3" transform="rotate(38.2273 494.397 522)" fill="white" fillOpacity="0.2" />
@@ -42,7 +44,7 @@ export const Square = () => {
                 <rect data-speed="-4" className="move" x="1559.29" y="226.475" width="29.5011" height="28.6062" rx="3" transform="rotate(42.6137 1559.29 226.475)" fill="white" fillOpacity="0.4" />
                 <rect data-speed="5" className="move" x="1455.99" y="614.993" width="23.8465" height="23.1231" rx="3" transform="rotate(52.3263 1455.99 614.993)" fill="white" fillOpacity="0.2" />
                 <rect data-speed="-9" className="move" x="1041.54" y="700" width="33.2816" height="32.2719" rx="3" transform="rotate(52.3263 1041.54 700)" fill="white" fillOpacity="0.4" />
-            </svg> */}
+            </svg>
         </div>
 
     )

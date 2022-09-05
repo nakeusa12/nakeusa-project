181e64be336aa20a1ba7706db9a25d7cbfb5fada
nakeusa12 <snakeusa8@gmail.com>
2022-09-05 19:14:41 +0700

    change meta

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Head>
         <title>Home</title>
-        <meta name="description" content="Home" />
+        <meta name="description" content="Personal Website Saiful Nakeusa" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
  

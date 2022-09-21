dc677ebc6090484f983f02184496a2e9f75fb93a
nakeusa12 <snakeusa8@gmail.com>
2022-09-21 23:25:43 +0700

    project

--- components/partials/Hero/index.jsx
+++ components/partials/Hero/index.jsx
@@ -1,13 +1,15 @@
+import Image from "next/image";
+
 export const Hero = () => {
   return (
     <section className="relative flex flex-col justify-center py-40">
       <div className="relative max-w-full md:max-w-[80%] mx-auto px-6 md:px-5 z-10">
         <div className="w-full block">
-          <span className="inline-block bg-main-blue py-2 px-5 xs:text-base sm:text-lg md:text-xl lg:text-2xl text-white font-medium uppercase tracking-[0.15rem]">
+          <span className="inline-block  bg-main-blue py-2 px-5 xs:text-base sm:text-lg md:text-xl lg:text-2xl text-white font-medium uppercase tracking-[0.15rem]">
             HI EVERYONE 👋
           </span>
         </div>
-        <div className="w-full mt-5">
+        <div className="mt-5 w-full">
           <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[5vw] text-main-dark xl:leading-[130%] tracking-wide">
             Be Creative In Building A Unique and Quality WebApps
           </h1>
@@ -28,19 +30,19 @@ export const Hero = () => {
             CONTENT CREATOR{" "}
           </p>
         </div>
-        <button className="py-3 md:py-4 px-10 md:px-12 inline-flex items-center justify-center border border-main-dark text-main-dark text-sm md:text-xl tracking-wider uppercase">
+        <button className="py-3 md:py-4 font-sen px-10 md:px-12 inline-flex items-center justify-center border border-main-dark text-main-dark text-sm md:text-xl tracking-wider uppercase">
           LET’S EXPLORE
         </button>
       </div>
 
-      {/* <div className="absolute right-0 top-1/3">
-        <img src="/assets/images/photo.png" className=" h-full w-full" alt="" />
-      </div> */}
-
-      <div>
-        <div className="absolute right-0 top-80 z-0 inline-block opacity-25 h-[900px] w-[828px]">
-          <img src="/assets/images/photo.png" alt="" style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center center', opacity: 1, transition: 'opacity 500ms ease 0s'}} />
-        </div>
+      <div className="absolute right-0 top-80 z-0 inline-block opacity-25 h-[1000px] w-[850px]">
+        {/* <img src="/assets/images/photo.png" alt="" style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center center', opacity: 1, transition: 'opacity 500ms ease 0s'}} /> */}
+        <Image
+          src="/assets/images/photo.png"
+          alt="Picture of the author"
+          width={850}
+          height={1000}
+        />
       </div>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,18 @@ import { Navbar } from "@components/partials/Navbar";
 import { BsArrowRight } from 'react-icons/bs';
 import Layout from "@components/partials/Layout";
 import Marquee from 'react-gsap-marquee'
+import { BsTriangleFill } from 'react-icons/bs';
+import Image from "next/image";
+import gsap from "gsap";
+import Project from "@components/partials/Project";
+// import ScrollTrigger from "gsap/dist/ScrollTrigger";
+// import ScrollToPlugin from "gsap/dist/ScrollToPlugin"
+// import { useEffect } from "react";
+
 // import { MasonryLayout } from "@components/basics/MasonryLayout";
 
 export default function Home() {
+
   return (
     <Layout title="Home">
         <Hero />
@@ -19,11 +28,11 @@ export default function Home() {
                 Quote <br /> <sup className="text-main-dark text-xl lg:text-2xl">Of The</sup> <span className="text-main-blue text-4xl lg:text-5xl">DAY</span>
               </div>
               <div className="w-full space-y-8">
-                <p className="font-semibold text-2xl sm:text-3xl lg:text-4xl text-main-dark leading-9 sm:leading-10 lg:leading-[48px] whitespace-normal">
+                <p className="font-bold font-sen text-2xl sm:text-3xl lg:text-4xl text-main-dark leading-9 sm:leading-10 lg:leading-[48px] whitespace-normal">
                   “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”
                 </p>
                 <div className="inline-flex gap-x-5 items-center">
-                  <p className="text-xl lg:text-2xl text-main-dark leading-8">Marilyn Monroe</p>
+                  <p className="text-xl lg:text-2xl text-main-dark leading-8 font-sen">Marilyn Monroe</p>
                   <div className="w-40 h-1 bg-main-blue"></div>
                 </div>
               </div>
@@ -42,10 +51,10 @@ export default function Home() {
             </div>
           </div>
           
-          <div className="w-full max-w-7xl mx-auto pt-20 grid grid-cols-12 gap-5">
-              <div className="col-span-5 w-full space-y-10">
-                <p className="font-nunito text-lg font-medium leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  <br /> <br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <button className="py-3 md:py-4 px-10 md:px-12 inline-flex items-center justify-center border border-main-dark text-main-dark text-sm md:text-xl tracking-wider uppercase">
+          <div className="w-full max-w-full md:max-w-[80%] mx-auto px-6 md:px-5 mt-20 grid grid-cols-12 gap-10 lg:gap-5">
+              <div className="col-span-12 lg:col-span-5 w-full space-y-10">
+                <p className="font-sen text-lg font-medium leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  <br /> <br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <button className="py-3 md:py-4 px-10 md:px-12 inline-flex items-center justify-center border border-main-dark text-main-dark text-sm md:text-xl tracking-wider uppercase font-sen">
                 Learn More
                 </button>
               </div>
@@ -107,18 +116,41 @@ export default function Home() {
                   </div>
                 </div>
               </div> */}
-              <div  className="col-span-7 col-start-9 w-full space-y-5">
-               <h3 className="text-main-dark uppercase font-poppins font-bold text-lg tracking-wider">SKILLS</h3>
-               <ul className="text-main-dark space-y-2.5">
-                <li>HTML - CSS - Javascript</li>
-                <li>React - Next Js</li>
-                <li>Svelte Js</li>
-                <li>Alphine Js</li>
-                <li>Tailwind CSS</li>
-                <li>Styled Components</li>
-                <li>SASS/SCSS</li>
-                <li>GSAP</li>
-               </ul>
+              <div  className="col-span-12 lg:col-span-6 col-start-1 lg:col-start-7 w-full gap-x-20 xl:gap-x-40 flex flex-row">
+                  <div className="inline-flex flex-row gap-x-6">
+                      <div className="inline-flex flex-col gap-y-2.5 pt-2">
+                        <BsTriangleFill className="w-4 h-4 rotate-0" />
+                        <BsTriangleFill className="w-4 h-4 rotate-0" />
+                        <BsTriangleFill className="w-4 h-4 rotate-0" />
+                      </div>
+                      <div>
+                        <h3 className="text-main-dark uppercase font-poppins font-bold text-lg tracking-wider">TECH STACK</h3>
+                        <ul className="text-main-dark space-y-4 font-sen mt-5 text-base">
+                          <li>HTML / CSS / Javascript</li>
+                          <li>React / Next JS</li>
+                          <li>Svelte / SvelteKit JS</li>
+                          <li>Alphine JS</li>
+                          <li>Tailwind CSS</li>
+                          <li>Styled Components</li>
+                        </ul>
+                      </div>
+                  </div>
+                  <div className="inline-flex flex-row gap-x-6">
+                      <div className="inline-flex flex-col gap-y-2.5 pt-2">
+                        <BsTriangleFill className="w-4 h-4 rotate-180" />
+                        <BsTriangleFill className="w-4 h-4 rotate-180" />
+                        <BsTriangleFill className="w-4 h-4 rotate-180" />
+                      </div>
+                      <div>
+                        <h3 className="text-main-dark uppercase font-poppins font-bold text-lg tracking-wider">ANIME STACK</h3>
+                        <ul className="text-main-dark space-y-4 font-sen mt-5 text-base">
+                          <li>Framer Motion</li>
+                          <li>GSAP</li>
+                          <li>Lottie JS</li>
+                          <li>ScrollReveal JS</li>
+                        </ul>
+                      </div>
+                  </div>
               </div>
           </div>
          
@@ -135,45 +167,17 @@ export default function Home() {
             </div>
           </div>
           
-          <div className="w-full max-w-7xl mx-auto pt-20">
-            <ul className="w-full flex flex-wrap mt-14 justify-between">
-              <li className="max-w-xl w-full h-full">
-                <div className="h-[60rem] w-full flex flex-col  justify-start">
-                  <div className="w-full bg-green-100 relative h-[40rem]">
-                    <img src="/assets/images/images.png" className="h-full w-full bg-blue-100" alt="" />
-                  </div>
-                  <div className="mt-10 space-y-4">
-                    <h3 className="font-bold text-black text-4xl font-poppins">BridgeZero - Website for work and visit visas</h3>
-                    <p className="font-poppins text-xl text-main-gray">2020 - Web Designer</p>
-                  </div>
-                </div>
-              </li>
-              <li className="max-w-xl w-full h-full">
-                <div className="h-[60rem] w-full flex flex-col justify-end">
-                  <div className="w-full bg-green-100 relative h-[40rem]">
-                    <img src="/assets/images/images.png" className="h-full w-full bg-blue-100" alt="" />
-                  </div>
-                  <div className="mt-10 space-y-4">
-                    <h3 className="font-bold text-black text-4xl font-poppins">BridgeZero - Website for work and visit visas</h3>
-                    <p className="font-poppins text-xl text-main-gray">2020 - Web Designer</p>
-                  </div>
-                </div>
-              </li>
-              <li className="max-w-xl w-full h-full">
-                <div className="h-[60rem] w-full flex flex-col  justify-start">
-                  <div className="w-full bg-green-100 relative h-[40rem]">
-                    <img src="/assets/images/images.png" className="h-full w-full bg-blue-100" alt="" />
-                  </div>
-                  <div className="mt-10 space-y-4">
-                    <h3 className="font-bold text-black text-4xl font-poppins">BridgeZero - Website for work and visit visas</h3>
-                    <p className="font-poppins text-xl text-main-gray">2020 - Web Designer</p>
-                  </div>
-                </div>
-              </li>
-              <li className="max-w-xl w-full h-full">
-                <div className="h-[60rem] w-full flex flex-col justify-end">
-                  <div className="w-full bg-green-100 relative h-[40rem]">
-                    <img src="/assets/images/images.png" className="h-full w-full bg-blue-100" alt="" />
+          {/* <div className="w-full max-w-full h-screen mt-20 bg-red-50">
+            <ul className="w-full whitespace-nowrap overflow-hidden">
+              <li className="inline-flex w-screen h-screen justify-center items-center">
+                <div className="w-full flex flex-col bg-green-100  justify-start">
+                  <div className="w-full relative">
+                    <Image
+                      src="/assets/images/images.png"
+                      alt="Picture of the author"
+                      width={650}
+                      height={700}
+                    />
                   </div>
                   <div className="mt-10 space-y-4">
                     <h3 className="font-bold text-black text-4xl font-poppins">BridgeZero - Website for work and visit visas</h3>
@@ -182,8 +186,8 @@ export default function Home() {
                 </div>
               </li>
             </ul>
-          </div>
-         
+          </div> */}
+          <Project />
         </div>
         <div className="w-full h-full bg-white py-40">
           <div className="relative w-full h-full">
